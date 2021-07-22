2746fc2052b6fca22fc2a4b65b344fcfbefb107e
SzczotkaKamil <szkamil1999@wp.pl>
2021-07-22 16:19:45 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kk_bud_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
